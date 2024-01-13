3df929f6ccb37ef929cc8532a63f8b0918d00ce5
po <po@po.com>
2024-01-13 12:07:14 +0700

    update

--- git-update.sh
+++ git-update.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
-
-# Menjalankan perintah git add
+GITHUB_USERNAME="farizxxx"
+GITHUB_TOKEN="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"
+REPO_NAME="embroiderylibraryprojects.com"
 git add .
-
-# Menjalankan perintah git commit
 git commit -m "update"
-
-# Menjalankan perintah git push
+git remote set-url origin https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/${GITHUB_USERNAME}/${REPO_NAME}.git
 git push -u origin main
-
-# Menampilkan pesan sebelum menutup jendela
 echo "Press any key to close the window"
 read -n 1 -s -r
